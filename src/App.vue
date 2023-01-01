1be9515c651ef40bc80dc2af8d97c2459b3116c5
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2023-01-01 22:02:53 +0200

    created landing page

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
-<script setup lang="ts"></script>
-
 <template>
-  <h1>Hello world!</h1>
+  <div class="container">
+    <h1>Welcome aboard! I am Abdelrhman Said</h1>
+
+    <p>
+      I am a full stack web developer, I have a passion for learning new
+      technologies and I am always looking for new challenges.
+    </p>
+
+    <a
+      href="https://docs.google.com/document/d/15uCs9LS0HaElbsARUxc1KcnKuVKLv-w5anl0oe5PjwU/edit?usp=share_link"
+      class="btn"
+      target="_blank"
+    >
+      Download my CV
+    </a>
+  </div>
 </template>
 
-<style scoped></style>
+<style>
+html,
+body {
+  display: grid;
+  place-items: center;
+}
+
+body::before {
+  content: '';
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-image: linear-gradient(90deg, #d946ef, #dc2626, #fb923c);
+}
+</style>
