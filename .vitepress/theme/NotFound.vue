c41b972da6842b39646b761b4747eb2bc0535712
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2024-01-19 23:24:03 +0200

    update not found design

--- .vitepress/theme/NotFound.vue
+++ .vitepress/theme/NotFound.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1 class="text-3xl font-bold">404 Page Not Found</h1>
+  <div class="flex flex-col items-center justify-center py-20">
+    <h1 class="text-4xl font-bold">404</h1>
+    <p class="text-xl">Page not found</p>
+  </div>
 </template>
