2a73be5fdf22113166ba22fcd18b71f53d07936f
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-01-05 13:48:22 +0200

    feat: add draft support and filter unpublished posts in posts data

--- .vitepress/theme/posts.data.ts
+++ .vitepress/theme/posts.data.ts
@@ -3,11 +3,12 @@ import { createContentLoader } from 'vitepress'
 export interface Post {
   title: string
   url: string
+  excerpt: string | undefined
+  draft?: boolean
   date: {
     time: number
     string: string
   }
-  excerpt: string | undefined
 }
 
 declare const data: Post[]
@@ -21,8 +22,10 @@ export default createContentLoader('posts/*.md', {
         title: frontmatter.title,
         url,
         excerpt,
+        draft: frontmatter.draft === true,
         date: formatDate(frontmatter.date)
       }))
+      .filter((post) => !post.draft)
       .sort((a, b) => b.date.time - a.date.time)
   }
 })
