b2d07b3560b6eda76520b17592a2b88bef7030c9
Abdelrhman Said <abdelrhmansaidzaki@hotmail.com>
2025-08-06 14:38:49 +0300

    remove nprogress

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,4 @@
 import './style.css'
-import 'nprogress/nprogress.css'
-import nprogress from 'nprogress'
 import type { EnhanceAppContext } from 'vitepress'
 
 import Layout from './Layout.vue'
@@ -8,23 +6,17 @@ import Layout from './Layout.vue'
 export default {
   Layout,
   enhanceApp({ router }: EnhanceAppContext) {
-    nprogress.configure({ showSpinner: false })
-
     setTimeout(() => {
       const cachedBeforeRouteChange = router.onBeforeRouteChange
       const cachedAfterRouteChange = router.onAfterRouteChanged
 
       router.onBeforeRouteChange = (to) => {
-        nprogress.start()
-
         if (cachedBeforeRouteChange) {
           cachedBeforeRouteChange(to)
         }
       }
 
       router.onAfterRouteChanged = (to) => {
-        nprogress.done()
-
         if (cachedAfterRouteChange) {
           cachedAfterRouteChange(to)
         }
